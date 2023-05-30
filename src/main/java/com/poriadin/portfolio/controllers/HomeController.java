0d8eca8c829f3f3cf4b4c2ae447421871f4b1ce0
Oleksandr Poriadin <ale.poryadin@gmail.com>
2023-05-30 21:42:27 +0300

    Deploy setting

---
+++ src/main/java/com/poriadin/portfolio/controllers/HomeController.java
@@ -0,0 +1,20 @@
+package com.poriadin.portfolio.controllers;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/home")
+public class HomeController {
+    @GetMapping("/")
+    public ResponseEntity<?> getAuthenticatedUser() {
+
+        return ResponseEntity.ok("HEEELLLO");
+    }
+
+}
